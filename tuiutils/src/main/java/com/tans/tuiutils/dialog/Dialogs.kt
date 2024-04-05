cc97621c1e78d67fd4ac7f81a1ef93a9bb5cfdc2
tans.tan <tans.tan096@gmail.com>
2024-04-05 22:30:00 +0800

    [feat] Remove default dialog navigation color param.

--- tuiutils/src/main/java/com/tans/tuiutils/dialog/Dialogs.kt
+++ tuiutils/src/main/java/com/tans/tuiutils/dialog/Dialogs.kt
@@ -4,27 +4,20 @@ import android.app.Activity
 import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import android.os.Bundle
-import android.text.Layout
-import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
-import androidx.annotation.StyleRes
-import androidx.appcompat.app.AppCompatDialog
-import com.tans.tuiutils.R
 import android.view.WindowManager.LayoutParams
-import android.view.accessibility.AccessibilityEvent
 import android.widget.FrameLayout
 import androidx.annotation.ColorInt
-import androidx.core.view.AccessibilityDelegateCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
+import androidx.annotation.StyleRes
+import androidx.appcompat.app.AppCompatDialog
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.sidesheet.SideSheetBehavior
 import com.google.android.material.sidesheet.SideSheetDialog
+import com.tans.tuiutils.R
 import com.tans.tuiutils.systembar.SystemBarThemeStyle
 import com.tans.tuiutils.systembar.systemBarThemeStyle
 
@@ -32,8 +25,6 @@ fun Activity.createDefaultDialog(
     contentView: View,
     isCancelable: Boolean = true,
     dimAmount: Float = 0.6f,
-    @ColorInt
-    navigationBarColor: Int = Color.TRANSPARENT,
     @StyleRes
     defaultTheme: Int = R.style.tUiUtils_BaseDialog,
     @StyleRes
@@ -78,7 +69,6 @@ fun Activity.createDefaultDialog(
         setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         setSoftInputMode(LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
         setWindowAnimations(windowAnima)
-        this.navigationBarColor = navigationBarColor
     }
     return dialog
 }
