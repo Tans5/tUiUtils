2e32fe0979a08f608371b3d6f37ca6b92573c6c9
tanpengcheng <tanpengcheng@inke.cn>
2024-07-24 14:15:15 +0800

    [feat] Add v 1.0.7

--- demo/src/main/java/com/tans/tuiutils/demo/TransparentSystemBarActivity.kt
+++ demo/src/main/java/com/tans/tuiutils/demo/TransparentSystemBarActivity.kt
@@ -1,14 +1,33 @@
 package com.tans.tuiutils.demo
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import com.tans.tuiutils.activity.BaseCoroutineStateActivity
 import com.tans.tuiutils.systembar.annotation.SystemBarStyle
+import kotlinx.coroutines.CoroutineScope
 
 @SystemBarStyle
-class TransparentSystemBarActivity : AppCompatActivity() {
+class TransparentSystemBarActivity : BaseCoroutineStateActivity<Unit>(Unit) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_transparent_system_bar)
+    override val layoutId: Int = R.layout.activity_transparent_system_bar
+
+    private val testArg: TestArg by lazyViewModelField("testArg") {
+        TestArg("TestArg: ${this.hashCode()}")
+    }
+
+    override fun CoroutineScope.firstLaunchInitDataCoroutine() {
+
+    }
+
+    override fun CoroutineScope.bindContentViewCoroutine(contentView: View) {
+
+    }
+
+    override fun onViewModelCleared() {
+        super.onViewModelCleared()
+        println(testArg)
+    }
+
+    companion object {
+        data class TestArg(val value: String)
     }
 }
